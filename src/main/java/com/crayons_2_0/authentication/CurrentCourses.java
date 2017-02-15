c1ae884739bd7bd0b1b249a050dabd0af33aacde
Ian <D064442@WDFN32916360A.emea.global.corp.sap>
2017-02-15 19:35:16 +0100

    Created CurrentCourses to get teh current curse

---
+++ src/main/java/com/crayons_2_0/authentication/CurrentCourses.java
@@ -0,0 +1,58 @@
+package com.crayons_2_0.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.crayons_2_0.model.Course;
+import com.crayons_2_0.service.database.CourseService;
+import com.vaadin.spring.annotation.SpringComponent;
+
+@SpringComponent
+public class CurrentCourses {
+
+	@Autowired
+	CourseService courseService;
+	private Course course;
+	private String title;
+	private static CurrentCourses instance = null;
+	 
+	
+	private CurrentCourses() {
+		 
+	}
+
+	public static CurrentCourses getInstance()
+	  {
+	    if (instance == null){
+	      instance = new CurrentCourses();
+	    }
+	    return instance;
+	}
+
+
+	public Course get() {
+
+		if (getInstance().getCourse() == null){
+			getInstance().setCourse(courseService.findCourseByTitle(getInstance().getTitle()));
+		}
+		return getInstance().getCourse();
+	}
+	
+	public Course getCourse() {
+
+		return course;
+	}
+	
+	public void setCourse(Course course) {
+
+		this.course = course;
+	}
+	public String getTitle() {
+
+		return title;
+	}
+	
+	public void setTitle(String title) {
+
+		this.title = title;
+	}
+}
