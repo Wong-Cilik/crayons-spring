2934bb4bf549fa1ee21708e37431bea5e0020482
Levin Schickle - ufebn <lschickle93@googlemail.com>
2017-03-07 22:46:53 +0100

    Some (not all!) Docs and stabilazion on course

--- src/main/java/com/crayons_2_0/model/Course.java
+++ src/main/java/com/crayons_2_0/model/Course.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.crayons_2_0.component.Unit;
 import com.crayons_2_0.model.graph.Graph;
+import com.crayons_2_0.service.database.CourseService;
 
 @SuppressWarnings("serial")
 @Component
@@ -26,6 +28,8 @@ public class Course implements Serializable {
 	private List<Unit> units = new ArrayList<Unit>();
 	private Date creationTime;
 	private Graph graph;
+	
+	private final int MAX_LENGTH_OF_TITLE = 30; // ????????????????????? 
 
 	public Course(String title, String description, CrayonsUser author) {
 		this.setTitle(title);
@@ -48,72 +52,157 @@ public class Course implements Serializable {
 	}
 
 	public Course(String title, CrayonsUser author) {
-		this.title = title;
+		this.setTitle(title);
 		this.setAuthor(author);
 		creationTime = new Date();
 	}
-
+	
+	
+	/**
+	 * Returns the students of course
+	 * @return
+	 */
 	public String getStudents() {
 		return students;
 	}
-
+	
+	/**
+	 * Sets the Studens of Course
+	 * @param students of course
+	 */
 	public void setStudents(String students) {
+		
+		//ToDo: Regex zur überprüfung
+		
 		this.students = students;
 	}
 
+	/**
+	 * Returns the title of course
+	 * @return title of course
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * Sets the Title of course
+	 * @param title to set
+	 */
 	public void setTitle(String title) {
+		if (title.isEmpty()) {
+			throw new IllegalArgumentException("Title of Course can't be empty");
+		}
+		
+		if (title.length() > MAX_LENGTH_OF_TITLE) {
+			throw new IllegalArgumentException("Title of Course is longer than allowed");
+		}
+		
 		this.title = title;
 	}
 
+	/**
+	 * Returns the description of Course
+	 * @return description of course
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Sets the description of course
+	 * @param description to set
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public Graph getGraph() {
-		return graph;
-	}
-
-	public void setGraph(Graph graph) {
-		this.graph = graph;
-	}
-
-	public Date getCreationTime() {
-		return creationTime;
-	}
-
-	public void setCreationTime(Date creationTime) {
-		this.creationTime = creationTime;
-	}
-
+	/**
+	 * Returns the author of course
+	 * @return the author of course
+	 */
 	public CrayonsUser getAuthor() {
 		return author;
 	}
 
+	/**
+	 * Sets the author of course
+	 * @param author to set
+	 */
 	public void setAuthor(CrayonsUser author) {
+		if (author == null) {
+			throw new IllegalArgumentException("Author of Course can't be Null");
+		}
+		
 		this.author = author;
 	}
 
-	public List<Unit> getUnits() {
-		return units;
-	}
-
-	public void setUnits(List<Unit> units) {
-		this.units = units;
-	}
-
+	/**
+	 * Returns the list of users of course
+	 * @return the users
+	 */
 	public List<CrayonsUser> getUsers() {
 		return users;
 	}
 
+	/**
+	 * Sets the list of users of course
+	 * @param users list of users to set
+	 */
 	public void setUsers(List<CrayonsUser> users) {
 		this.users = users;
 	}
+
+	/**
+	 * Returns the units of course
+	 * @return the units of course
+	 */
+	public List<Unit> getUnits() {
+		return units;
+	}
+
+	/**
+	 * @param units the units to set
+	 */
+	public void setUnits(List<Unit> units) {
+		this.units = units;
+	}
+
+	/**
+	 * 
+	 * @return the creationTime
+	 */
+	public Date getCreationTime() {
+		return creationTime;
+	}
+
+	/**
+	 * @param creationTime the creationTime to set
+	 */
+	public void setCreationTime(Date creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	/**
+	 * @return the graph
+	 */
+	public Graph getGraph() {
+		return graph;
+	}
+
+	/**
+	 * @param graph the graph to set
+	 */
+	public void setGraph(Graph graph) {
+		this.graph = graph;
+	}
+
+	/**
+	 * @return the MAX_LENGTH_OF_TITLE
+	 */
+	public int getMAX_LENGTH_OF_TITLE() {
+		return MAX_LENGTH_OF_TITLE;
+	}
+
+	
 }
