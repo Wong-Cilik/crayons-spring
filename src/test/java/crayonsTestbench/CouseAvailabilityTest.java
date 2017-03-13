2739bd00e889bd9e230674a79015b5b070c13e27
Natalia Krylova <n.k.krylova@gmail.com>
2017-03-13 06:46:25 +0100

    Test for selecting and removing participants of the course.

--- src/test/java/crayonsTestbench/CouseAvailabilityTest.java
+++ src/test/java/crayonsTestbench/CouseAvailabilityTest.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,6 +17,7 @@ import com.crayons_2_0.service.LanguageService;
 import com.vaadin.testbench.TestBench;
 import com.vaadin.testbench.TestBenchTestCase;
 import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.LabelElement;
 import com.vaadin.testbench.elements.MenuBarElement;
 import com.vaadin.testbench.elements.PasswordFieldElement;
 import com.vaadin.testbench.elements.TabSheetElement;
@@ -43,90 +46,156 @@ public class CouseAvailabilityTest extends TestBenchTestCase {
         getDriver().get("http://localhost:8080");
         driver.manage().window().maximize();
     }
-
+    
     @After
     public void tearDown() throws Exception {
-        // close the browser window
+     // close the browser window
         getDriver().quit();
     }
 
     @Test
-    public void test() {
+    public void testAddAndRemoveParticipant() {
         
-        // 1. Enter "client@web.de" into the Email field
-        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
-                .setValue("client@web.de");
-        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
-                .setValue("123456");
-        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
+        // 1. Log in as "client@web.de" and create new course "Algebra"
+        logIn("client@web.de", "123456");
         $(ButtonElement.class).caption(lang.getString("Authorlibrary")).first().click();
         $(TabSheetElement.class).$(TextFieldElement.class).first().setValue("Algebra");
         $(TextAreaElement.class).first().setValue("Algebra is one of the broad parts of mathematics.");
         $(ButtonElement.class).caption(lang.getString("CreateCourse")).first().click();
         
+        // 2. Open the tab of the "Algebra" course
+        $(TabSheetElement.class).first().openTab("Algebra");
+        
+        // 3. Select "student@mail.com" from the List of all students and move them to the Participants
         $(TwinColSelectElement.class).caption(lang.getString("SelectParticipants"))
                 .first().selectByText("student@mail.com");
         
+        // 4. Click "Save Students" to save course participants
         $(ButtonElement.class).caption(lang.getString("SaveStudents")).first().click();
         
+        // 5. Click "Logout"
         $(MenuBarElement.class).first().click();
         
-        // 1. Enter "student@mail.com" into the Email field
-        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
-                .setValue("student@mail.com");
-        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
-                .setValue("123456");
-        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
+        // 6. Log in as "student@mail.com"
+        logIn("student@mail.com", "123456");
         
-        // 4. Click "Userlibrary" to navigate to the author library
+        // 7. Click "Userlibrary" to navigate to the user library
         $(ButtonElement.class).caption(lang.getString("Userlibrary")).first().click();
         
+        // 8. Assert that the TabSheet contains "Algera". It means that the course became available
+        // for the student
         assertTrue($(TabSheetElement.class).first().getTabCaptions().contains("Algebra"));
         
+        // 9. Click "Logout"
         $(MenuBarElement.class).first().click();
         
-        // 1. Enter "client@web.de" into the Email field
-        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
-                .setValue("client@web.de");
-        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
-                .setValue("123456");
-        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
+        // 10. Log in as "client@web.de" and navigate to the author library
+        logIn("client@web.de", "123456");
         $(ButtonElement.class).caption(lang.getString("Authorlibrary")).first().click();
         
+        // 11. Open the tab of the "Algebra" course
         $(TabSheetElement.class).first().openTab("Algebra");
         
+        // 12. Select "student@mail.com" from the Participants and move them to the List of all students 
         $(TwinColSelectElement.class).caption(lang.getString("SelectParticipants"))
                 .first().deselectByText("student@mail.com");
         
+        // 13. Click "Save Students" to save course participants
         $(ButtonElement.class).caption(lang.getString("SaveStudents")).first().click();
         
+        // 14. Click "Logout"
         $(MenuBarElement.class).first().click();
         
-        // 1. Enter "student@mail.com" into the Email field
-        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
-                .setValue("student@mail.com");
-        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
-                .setValue("123456");
-        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
-        
-        // 4. Click "Userlibrary" to navigate to the author library
+        // 15. Log in as "student@mail.com" and navigate to the user library
+        logIn("student@mail.com", "123456");
         $(ButtonElement.class).caption(lang.getString("Userlibrary")).first().click();
         
+        // 16. Assert that the TabSheet doesn't contain "Algebra" anymore. It means that the course 
+        // became unavailable for the student
         if ($(TabSheetElement.class).first() != null)
             assertFalse($(TabSheetElement.class).first().getTabCaptions().contains("Algebra"));
         
+        // 17. Click "Logout"
         $(MenuBarElement.class).first().click();
         
-        // 1. Enter "client@web.de" into the Email field
-        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
-                .setValue("client@web.de");
-        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
-                .setValue("123456");
-        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
+        // 18. Log in as "client@web.de" and delete the course "Algebra"
+        logIn("client@web.de", "123456");
         $(ButtonElement.class).caption(lang.getString("Authorlibrary")).first().click();
+        $(TabSheetElement.class).first().openTab("Algebra");
+        $(ButtonElement.class).caption(lang.getString("DeleteCourse")).first().click();
+        $(MenuBarElement.class).first().click();
+    }
+    
+    @Test
+    public void testParticipantLeavesTheCourse() {
         
+        // 1. Log in as "client@web.de" and create new course "Algebra"
+        logIn("client@web.de", "123456");
+        $(ButtonElement.class).caption(lang.getString("Authorlibrary")).first().click();
+        $(TabSheetElement.class).$(TextFieldElement.class).first().setValue("Algebra");
+        $(TextAreaElement.class).first().setValue("Algebra is one of the broad parts of mathematics.");
+        $(ButtonElement.class).caption(lang.getString("CreateCourse")).first().click();
+        
+        // 2. Open the tab of the "Algebra" course
         $(TabSheetElement.class).first().openTab("Algebra");
         
+        // 3. Select "student@mail.com" from the List of all students and move them to the Participants
+        $(TwinColSelectElement.class).caption(lang.getString("SelectParticipants"))
+                .first().selectByText("student@mail.com");
+        
+        // 4. Click "Save Students" to save course participants
+        $(ButtonElement.class).caption(lang.getString("SaveStudents")).first().click();
+        
+        // 5. Click "Logout"
+        $(MenuBarElement.class).first().click();
+        
+        // 6. Log in as "student@mail.com"
+        logIn("student@mail.com", "123456");
+        
+        // 7. Click "Userlibrary" to navigate to the user library
+        $(ButtonElement.class).caption(lang.getString("Userlibrary")).first().click();
+        
+        // 8. Assert that the TabSheet contains "Algera". It means that the course became available
+        // for the student
+        assertTrue($(TabSheetElement.class).first().getTabCaptions().contains("Algebra"));
+        
+        // 9. Open the tab of the "Algebra" course
+        $(TabSheetElement.class).first().openTab("Algebra");
+        
+        // 10. Click "Leave the course" 
+        $(ButtonElement.class).caption(lang.getString("LeaveTheCourse")).first().click();
+        
+        // 11. Assert that the TabSheet doesn't contain "Algebra" anymore. 
+        if ($(TabSheetElement.class).first() != null)
+            assertFalse($(TabSheetElement.class).first().getTabCaptions().contains("Algebra"));
+        
+        // 12. Click "Logout"
+        $(MenuBarElement.class).first().click();
+        
+        // 13. Log in as "client@web.de" and navigate to the author library
+        logIn("client@web.de", "123456");
+        $(ButtonElement.class).caption(lang.getString("Authorlibrary")).first().click();
+        
+        // 14. Open the tab of the "Algebra" course
+        $(TabSheetElement.class).first().openTab("Algebra");
+        
+        
+        // 15. Assert that the participants list doesn't contain "student@mail.com" anymore 
+        assertFalse($(TwinColSelectElement.class).caption(lang.getString("SelectParticipants"))
+                .first().getValues().contains("student@mail.com"));
+        
+        // 16. Click "Delete Course"
         $(ButtonElement.class).caption(lang.getString("DeleteCourse")).first().click();
+        
+        // 17. Click "Logout"
+        $(MenuBarElement.class).first().click();
+    }
+    
+    private void logIn(String email, String password) {
+        $(TextFieldElement.class).caption(lang.getString("Login") + ": ").first()
+                .setValue(email);
+        $(PasswordFieldElement.class).caption(lang.getString("Password") + ": ").first()
+                .setValue(password);
+        $(ButtonElement.class).caption(lang.getString("Login")).first().click();
     }
 }
